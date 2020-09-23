174371ed5e1d76fe1a229fe7a642f1daf6ddee1a
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-23 21:10:50 +0700

    add info in file footer.php

--- wp-content/themes/learn_theme/footer.php
+++ wp-content/themes/learn_theme/footer.php
@@ -1,2 +1,7 @@
+</main>
+<footer>
+    <p>&copy; Copyrights <a href="<?php bloginfo('url');?>"><?php bloginfo('name');?></a>.All Rights Reserved</p>
+</footer>
+<?php wp_footer()?>
 </body>
 </html>
